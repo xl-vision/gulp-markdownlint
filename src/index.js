f5d4663cf5ee470be734b4ecf9d916fafc249135
Rhys Xia <xrs4433@outlook.com>
2019-04-06 22:02:03 +0800

    :sparkle: enhance functions

--- src/index.js
+++ src/index.js
@@ -4,15 +4,23 @@ const MarkdownLint = require('markdownlint')
 const path = require('path')
 const pluginName = 'gulp-markdownlint'
 
-module.exports = function gulpMarkdownlint(options) {
+module.exports = function gulpMarkdownlint(options = {}) {
 
     let configFile = options.configFile || 'markdownlint.json'
+
+    const formatter = options.formatter || 'verbose'
+
+    const allowWarnings = options.allowWarnings || false
+
+    delete options.configFile
+    delete options.formatter
+    delete options.allowWarnings
+
 
     if (!path.isAbsolute(configFile)) {
         configFile = path.join(process.cwd(), configFile)
     }
 
-    delete options.configFile
 
     let config = MarkdownLint.readConfigSync(configFile)
 
@@ -43,15 +51,66 @@ module.exports = function gulpMarkdownlint(options) {
             if (err) {
                 this.emit('error', new PluginError(pluginName, err))
             } else {
-                const message = ret.toString(true)
+                const errorMessage = ret.toString(true)
+                if (errorMessage) {
+                    const message = formatResult(ret, formatter)
 
-                if (message) {
-                    this.emit('error', new PluginError(pluginName, message))
+                    if (allowWarnings) {
+                        console.warn(message)
+                    } else {
+                        console.error(message)
+                    }
+                    this.emit('error', new PluginError(pluginName, errorMessage))
                 }
+
             }
             done()
         })
     }
 
     return through2.obj(onFile, onStreamEnd).resume()
+}
+
+
+function formatResult(result, formatter) {
+
+    if (formatter === 'json') {
+        return JSON.stringify(result)
+    }
+    if (formatter === 'verbose') {
+        const files = Object.keys(result)
+        const messageArr = []
+        for (const file of files) {
+            const arr = result[file]
+            if (!arr && arr.length === 0) {
+                continue
+            }
+            const tempArr = []
+            for (const item of arr) {
+                const lineNumber = item['lineNumber']
+                const ruleNames = item['ruleNames']
+                const ruleDescription = item['ruleDescription']
+                const ruleInformation = item['ruleInformation']
+                const errorContext = item['errorContext']
+                const errorDetail = item['errorDetail']
+                const errorRange = item['errorRange']
+
+                let message = `\n\tline ${lineNumber}`
+                if (errorRange) {
+                    message += `,start ${errorRange[0]},end ${errorRange[1]})`
+                }
+                message += `: ${errorContext}`
+                message += `\n\t${ruleNames[0]}: ${ruleDescription} (${ruleInformation})`
+                if (errorDetail) {
+                    message += `\n\tmessage: ${errorDetail}`
+                }
+
+                tempArr.push(message)
+            }
+            messageArr.push(`${file}:${tempArr.join('\n')}`)
+        }
+
+        return messageArr.join('\n\n')
+    }
+    return ''
 }
--- test/index.js
+++ test/index.js
@@ -6,17 +6,94 @@ const resolve = glob => path.join(__dirname, 'files', glob)
 
 
 describe('test index', () => {
-    it('should not emit error when file is right', () => {
+    it('check error message', () => {
+        expect.assertions(1)
         return new Promise((resolved) => {
             gulp.src(resolve('base.md'))
                 .pipe(gulpMarkdownlint({
                     configFile: resolve('markdownlint.json')
                 }))
                 .on('error', err => {
-                    expect(err.message).toMatch(/MD041/)
+                    expect(err.message.replace(/.*base.md/g, '')).toMatchSnapshot()
                     resolved()
                 })
 
+        })
+
+    })
+
+    it('check console message with formater: verbose', () => {
+        expect.assertions(1)
+        jest.spyOn(console, 'error')
+            .mockImplementation(message => {
+                expect(message.replace(/^.*\n/, '')).toMatchSnapshot()
+            })
+
+
+        return new Promise((resolved) => {
+            gulp.src(resolve('base.md'))
+                .pipe(gulpMarkdownlint({
+                    configFile: resolve('markdownlint.json'),
+                    formatter: 'verbose'
+                }))
+                .on('error', () => resolved())
+        })
+
+    })
+
+    it('check console message with formater: json', () => {
+        expect.assertions(1)
+        jest.spyOn(console, 'error')
+            .mockImplementation(message => {
+                expect(message.replace(/".*base.md/g, '')).toMatchSnapshot()
+            })
+
+
+        return new Promise((resolved) => {
+            gulp.src(resolve('base.md'))
+                .pipe(gulpMarkdownlint({
+                    configFile: resolve('markdownlint.json'),
+                    formatter: 'json'
+                }))
+                .on('error', () => resolved())
+        })
+
+    })
+
+    it('check console message with formater: none', () => {
+        expect.assertions(1)
+        jest.spyOn(console, 'error')
+            .mockImplementation(message => {
+                expect(message).toBe('')
+            })
+
+
+        return new Promise((resolved) => {
+            gulp.src(resolve('base.md'))
+                .pipe(gulpMarkdownlint({
+                    configFile: resolve('markdownlint.json'),
+                    formatter: 'none'
+                }))
+                .on('error', () => resolved())
+        })
+
+    })
+
+    it('check console type with allowWarnings: true', () => {
+        expect.assertions(1)
+        jest.spyOn(console, 'warn')
+            .mockImplementation(message => {
+                expect(message.replace(/^.*\n/, '')).toMatchSnapshot()
+            })
+
+
+        return new Promise((resolved) => {
+            gulp.src(resolve('base.md'))
+                .pipe(gulpMarkdownlint({
+                    configFile: resolve('markdownlint.json'),
+                    allowWarnings: true
+                }))
+                .on('error', () => resolved())
         })
 
     })
