d856d984e5d9a4e12b70a2e42ea008bfc761483c
Rhys Xia <xrs4433@outlook.com>
2019-04-08 10:53:37 +0800

    :rotating_lint:fixed lint warnings

--- src/index.js
+++ src/index.js
@@ -5,126 +5,122 @@ const path = require('path')
 const pluginName = 'gulp-markdownlint'
 const fs = require('fs')
 
-module.exports = function gulpMarkdownlint(options = {}) {
+module.exports = function gulpMarkdownlint (options = {}) {
+  let configFile = options.configFile || 'markdownlint.json'
 
-    let configFile = options.configFile || 'markdownlint.json'
+  let config = options.config || {}
 
-    let config = options.config || {}
-
-    if(configFile && fs.existsSync(configFile)){
-        if (!path.isAbsolute(configFile)) {
-            configFile = path.join(process.cwd(), configFile)
-        }
-        const tempConfig = MarkdownLint.readConfigSync(configFile)
-        config = {...tempConfig, ...config}
+  if (configFile && fs.existsSync(configFile)) {
+    if (!path.isAbsolute(configFile)) {
+      configFile = path.join(process.cwd(), configFile)
     }
+    const tempConfig = MarkdownLint.readConfigSync(configFile)
+    config = { ...tempConfig, ...config }
+  }
 
-    const formatter = options.formatter || 'verbose'
+  const formatter = options.formatter || 'verbose'
 
-    const failAfterError = options.failAfterError === undefined ? true : options.failAfterError
+  const failAfterError = options.failAfterError === undefined ? true : options.failAfterError
 
-    delete options.configFile
-    delete options.formatter
-    delete options.failAfterError
-    delete options.config
+  delete options.configFile
+  delete options.formatter
+  delete options.failAfterError
+  delete options.config
 
-    const files = []
+  const files = []
 
-    function onFile(file, encoding, done) {
-        if (file.isNull()) {
-            done(null, file)
-            return
-        }
-        if (file.isStream()) {
-            this.emit('error', new PluginError(pluginName, 'Streaming is not supported'))
-            done()
-            return
-        }
-        const filePath = file.path
-
-        files.push(filePath)
-        done(null, file)
+  function onFile (file, encoding, done) {
+    if (file.isNull()) {
+      done(null, file)
+      return
     }
-
-    function onStreamEnd(done) {
-        MarkdownLint({
-            ...options,
-            files: files,
-            config: config
-        }, (err, ret) => {
-            if (err) {
-                this.emit('error', new PluginError(pluginName, err))
-            } else {
-                const allMessage = ret.toString(true)
-
-                const message = formatResult(ret, formatter)
-                console.info(message)
-
-                if(failAfterError && allMessage) {
-                    this.emit('error', new PluginError(pluginName, allMessage))
-                }
-
-            }
-            done()
-        })
+    if (file.isStream()) {
+      this.emit('error', new PluginError(pluginName, 'Streaming is not supported'))
+      done()
+      return
     }
+    const filePath = file.path
 
-    return through2.obj(onFile, onStreamEnd).resume()
+    files.push(filePath)
+    done(null, file)
+  }
+
+  function onStreamEnd (done) {
+    MarkdownLint({
+      ...options,
+      files: files,
+      config: config
+    }, (err, ret) => {
+      if (err) {
+        this.emit('error', new PluginError(pluginName, err))
+      } else {
+        const allMessage = ret.toString(true)
+
+        const message = formatResult(ret, formatter)
+        console.info(message)
+
+        if (failAfterError && allMessage) {
+          this.emit('error', new PluginError(pluginName, allMessage))
+        }
+      }
+      done()
+    })
+  }
+
+  return through2.obj(onFile, onStreamEnd).resume()
 }
-
 
 // 过滤掉没有错误的结果
-function filterResult(result){
-    const obj = {}
-    const files = Object.keys(result)
-    for (const file of files) {
-        const arr = result[file]
-        if(!arr || arr.length === 0){
-            continue
-        }
-        obj[file] = arr
+function filterResult (result) {
+  const obj = {}
+  const files = Object.keys(result)
+  for (const file of files) {
+    const arr = result[file]
+    if (!arr || arr.length === 0) {
+      continue
     }
-    return obj
+    obj[file] = arr
+  }
+  return obj
 }
 
-function formatResult(result, formatter) {
+function formatResult (result, formatter) {
+  result = filterResult(result)
 
-    result = filterResult(result)
+  if (formatter === 'json') {
+    return JSON.stringify(result)
+  }
+  if (formatter === 'verbose') {
+    const files = Object.keys(result)
+    const messageArr = []
+    for (const file of files) {
+      const arr = result[file]
+      const tempArr = []
+      for (const item of arr) {
+        const lineNumber = item['lineNumber']
+        const ruleNames = item['ruleNames']
+        const ruleDescription = item['ruleDescription']
+        const ruleInformation = item['ruleInformation']
+        const errorContext = item['errorContext']
+        const errorDetail = item['errorDetail']
+        const errorRange = item['errorRange']
 
-    if (formatter === 'json') {
-        return JSON.stringify(result)
-    }
-    if (formatter === 'verbose') {
-        const files = Object.keys(result)
-        const messageArr = []
-        for (const file of files) {
-            const arr = result[file]
-            const tempArr = []
-            for (const item of arr) {
-                const lineNumber = item['lineNumber']
-                const ruleNames = item['ruleNames']
-                const ruleDescription = item['ruleDescription']
-                const ruleInformation = item['ruleInformation']
-                const errorContext = item['errorContext']
-                const errorDetail = item['errorDetail']
-                const errorRange = item['errorRange']
-
-                let message = `\n\tline ${lineNumber}`
-                if (errorRange) {
-                    message += `,start ${errorRange[0]},end ${errorRange[1]})`
-                }
-                message += `: ${errorContext}`
-                message += `\n\t${ruleNames[0]}: ${ruleDescription} (${ruleInformation})`
-                if (errorDetail) {
-                    message += `\n\tmessage: ${errorDetail}`
-                }
-
-                tempArr.push(message)
-            }
-            messageArr.push(`${file}:${tempArr.join('\n')}`)
+        let message = `\n\tline ${lineNumber}`
+        if (errorRange) {
+          message += `,start ${errorRange[0]},end ${errorRange[1]})`
+        }
+        message += `: ${errorContext}`
+        message += `\n\t${ruleNames[0]}: ${ruleDescription} (${ruleInformation})`
+        if (errorDetail) {
+          message += `\n\tmessage: ${errorDetail}`
         }
 
-        return messageArr.join('\n\n')
+        tempArr.push(message)
+      }
+      messageArr.push(`${file}:${tempArr.join('\n')}`)
     }
-    return ''
+
+    return messageArr.join('\n\n')
+  }
+  return ''
 }
--- test/index.js
+++ test/index.js
@@ -4,137 +4,130 @@ const gulpMarkdownlint = require('../src')
 
 const resolve = glob => path.join(__dirname, 'files', glob)
 
-
 describe('test index', () => {
-
-    it('check error message', () => {
-        expect.assertions(1)
-        return new Promise((resolved) => {
-            gulp.src(resolve('base.md'))
-                .pipe(gulpMarkdownlint({
-                    configFile: resolve('markdownlint.json')
-                }))
-                .on('error', err => {
-                    expect(err.message.replace(/.*base.md/g, '')).toMatchSnapshot()
-                    resolved()
-                })
-
-        })
-
-    })
-
-    it('check console message with formater: verbose', () => {
-        expect.assertions(1)
-        jest.spyOn(console, 'info')
-            .mockImplementation(message => {
-                expect(message.replace(/^.*\n/, '')).toMatchSnapshot()
-            })
-
-
-        return new Promise((resolved) => {
-            gulp.src(resolve('base.md'))
-                .pipe(gulpMarkdownlint({
-                    configFile: resolve('markdownlint.json'),
-                    formatter: 'verbose'
-                }))
-                .on('error', () => resolved())
-        })
-
-    })
-
-    it('check console message with formater: json', () => {
-        expect.assertions(1)
-        jest.spyOn(console, 'info')
-            .mockImplementation(message => {
-                expect(message.replace(/".*base.md/g, '')).toMatchSnapshot()
-            })
-
-
-        return new Promise((resolved) => {
-            gulp.src(resolve('base.md'))
-                .pipe(gulpMarkdownlint({
-                    configFile: resolve('markdownlint.json'),
-                    formatter: 'json'
-                }))
-                .on('error', () => resolved())
-        })
-
-    })
-
-    it('check console message with formater: none', () => {
-        expect.assertions(1)
-        jest.spyOn(console, 'info')
-            .mockImplementation(message => {
-                expect(message).toBe('')
-            })
-
-        return new Promise((resolved) => {
-            gulp.src(resolve('base.md'))
-                .pipe(gulpMarkdownlint({
-                    configFile: resolve('markdownlint.json'),
-                    formatter: 'none'
-                }))
-                .on('error', () => {
-                    resolved()
-                })
-        })
-
-    })
-
-    // it('check error with failAfterError: true', () => {
-    //     expect.assertions(1)
-    //     return new Promise((resolved) => {
-    //         gulp.src(resolve('base.md'))
-    //             .pipe(gulpMarkdownlint({
-    //                 configFile: resolve('markdownlint.json'),
-    //                 failAfterError: true
-    //             }))
-    //             .on('error', err => {
-    //                 console.log(err)
-    //                 expect(err.message.replace(/.*base.md/g, '')).toMatchSnapshot()
-    //                 resolved()
-    //             })
-
-    //     })
-
-    // })
-
-    it('check error with failAfterError: false', () => {
-        expect.assertions(0)
-        return new Promise((resolved) => {
-            gulp.src(resolve('base.md'))
-                .pipe(gulpMarkdownlint({
-                    configFile: resolve('markdownlint.json'),
-                    failAfterError: false
-                }))
-                .on('error', err => {
-                    expect(err.message.replace(/.*base.md/g, '')).toMatchSnapshot()
-                    resolved()
-                })
-                .on('end',resolved)
-
+  it('check error message', () => {
+    expect.assertions(1)
+    return new Promise((resolve) => {
+      gulp.src(resolve('base.md'))
+        .pipe(gulpMarkdownlint({
+          configFile: resolve('markdownlint.json')
+        }))
+        .on('error', err => {
+          expect(err.message.replace(/.*base.md/g, '')).toMatchSnapshot()
+          resolve()
         })
     })
+  })
 
+  it('check console message with formater: verbose', () => {
+    expect.assertions(1)
+    jest.spyOn(console, 'info')
+      .mockImplementation(message => {
+        expect(message.replace(/^.*\n/, '')).toMatchSnapshot()
+      })
 
-    it('provide config object instead of config file', () => {
-        expect.assertions(1)
-        jest.spyOn(console, 'info')
-            .mockImplementation(message => {
-                expect(message.replace(/^.*\n/, '')).toMatchSnapshot()
-            })
-
-        return new Promise((resolved) => {
-            gulp.src(resolve('base.md'))
-                .pipe(gulpMarkdownlint({
-                    configFile: false,
-                    config: {
-                        MD022: {lines_above: 0, lines_below: 0}
-                    },
-                    allowWarnings: true
-                }))
-                .on('error', () => resolved())
-        })
-
+    // eslint-disable-next-line promise/param-names
+    return new Promise((resolved) => {
+      gulp.src(resolve('base.md'))
+        .pipe(gulpMarkdownlint({
+          configFile: resolve('markdownlint.json'),
+          formatter: 'verbose'
+        }))
+        .on('error', () => resolved())
     })
+  })
+
+  it('check console message with formater: json', () => {
+    expect.assertions(1)
+    jest.spyOn(console, 'info')
+      .mockImplementation(message => {
+        expect(message.replace(/".*base.md/g, '')).toMatchSnapshot()
+      })
+
+    // eslint-disable-next-line promise/param-names
+    return new Promise((resolved) => {
+      gulp.src(resolve('base.md'))
+        .pipe(gulpMarkdownlint({
+          configFile: resolve('markdownlint.json'),
+          formatter: 'json'
+        }))
+        .on('error', () => resolved())
+    })
+  })
+
+  it('check console message with formater: none', () => {
+    expect.assertions(1)
+    jest.spyOn(console, 'info')
+      .mockImplementation(message => {
+        expect(message).toBe('')
+      })
+
+    // eslint-disable-next-line promise/param-names
+    return new Promise((resolved) => {
+      gulp.src(resolve('base.md'))
+        .pipe(gulpMarkdownlint({
+          configFile: resolve('markdownlint.json'),
+          formatter: 'none'
+        }))
+        .on('error', () => {
+          resolved()
+        })
+    })
+  })
+
+  // it('check error with failAfterError: true', () => {
+  //   expect.assertions(1)
+  //   return new Promise((resolved) => {
+  //     gulp.src(resolve('base.md'))
+  //       .pipe(gulpMarkdownlint({
+  //         configFile: resolve('markdownlint.json'),
+  //         failAfterError: true
+  //       }))
+  //       .on('error', err => {
+  //         console.log(err)
+  //         expect(err.message.replace(/.*base.md/g, '')).toMatchSnapshot()
+  //         resolved()
+  //       })
+
+  //   })
+
+  // })
+
+  it('check error with failAfterError: false', () => {
+    expect.assertions(0)
+    // eslint-disable-next-line promise/param-names
+    return new Promise((resolved) => {
+      gulp.src(resolve('base.md'))
+        .pipe(gulpMarkdownlint({
+          configFile: resolve('markdownlint.json'),
+          failAfterError: false
+        }))
+        .on('error', err => {
+          expect(err.message.replace(/.*base.md/g, '')).toMatchSnapshot()
+          resolved()
+        })
+        .on('end', resolved)
+    })
+  })
+
+  it('provide config object instead of config file', () => {
+    expect.assertions(1)
+    jest.spyOn(console, 'info')
+      .mockImplementation(message => {
+        expect(message.replace(/^.*\n/, '')).toMatchSnapshot()
+      })
+
+    // eslint-disable-next-line promise/param-names
+    return new Promise((resolved) => {
+      gulp.src(resolve('base.md'))
+        .pipe(gulpMarkdownlint({
+          configFile: false,
+          config: {
+            MD022: { lines_above: 0, lines_below: 0 }
+          },
+          allowWarnings: true
+        }))
+        .on('error', () => resolved())
+    })
+  })
 })
